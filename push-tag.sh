2e50e0e613a17dae342a2e3b28b0332333e90990
shaalx <wisdomswing@gmail.com>
2015-07-22 17:31:56 +0800

    bash

--- push-tag.sh
+++ push-tag.sh
@@ -3,4 +3,7 @@ git status
 @echo "tag:"
 read commits
 git tag -a "$commits" -m "$commits"
-git push origin --tags
+git push origin --tag $commits:$commits
+sleep 10
+git tag -d $commits
+git push origin --tag :$commits
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar target/gs-rest-service-0.1.0.jar
